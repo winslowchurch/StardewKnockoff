e166352681393224a0b726c8154c4d2277041700
Winslow Church <winslowc@stanford.edu>
2024-11-13 10:16:33 -0800

    update images

--- data/Tilesets/Objects.tsx
+++ data/Tilesets/Objects.tsx
@@ -5,22 +5,10 @@
   <image width="64" height="60" source="../../graphics/objects/bush.png"/>
  </tile>
  <tile id="1">
-  <image width="56" height="68" source="../../graphics/objects/merchant.png"/>
- </tile>
- <tile id="2">
-  <image width="40" height="40" source="../../graphics/objects/stump_medium.png"/>
- </tile>
- <tile id="3">
-  <image width="32" height="40" source="../../graphics/objects/stump_small.png"/>
+  <image width="56" height="68" source="../../graphics/objects/girlfriend.png"/>
  </tile>
  <tile id="4">
   <image width="56" height="112" source="../../graphics/objects/sunflower.png"/>
- </tile>
- <tile id="5">
-  <image width="96" height="124" source="../../graphics/objects/tree_medium.png"/>
- </tile>
- <tile id="6">
-  <image width="56" height="116" source="../../graphics/objects/tree_small.png"/>
  </tile>
  <tile id="7">
   <image width="44" height="48" source="../../graphics/objects/flower.png"/>
