46a3826af52741b61d4c573dde90a00b59e8ace0
Winslow Church <winslowc@stanford.edu>
2024-11-14 10:06:24 -0800

    audio

--- code/player.py
+++ code/player.py
@@ -67,7 +67,7 @@ class Player(pygame.sprite.Sprite):
         self.toggle_shop = toggle_shop
 
         # sound
-        self.watering = pygame.mixer.Sound('../audio/water.mp3')
+        self.watering = pygame.mixer.Sound('../audio/water.wav')
         self.watering.set_volume(0.2)
 
     def use_tool(self):
